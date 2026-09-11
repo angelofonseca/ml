2c7dddc76dbdf2acb5cc74d4d56c41bc7c9b61ec
angelofonseca <angeloniemeyer97@gmail.com>
2026-09-11 08:45:51 -0300

    ajusta o o predict do classification report e faz o dump do scaler junto

--- model.py
+++ model.py
@@ -2,10 +2,9 @@ import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
-from imblearn.pipeline import Pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("Cancer_Data.csv")
 df.drop("Unnamed: 32", axis=1, inplace=True)
@@ -18,15 +17,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=1, stratify=y)
 
 scaler = StandardScaler()
-X_train_fit = scaler.fit_transform(X_train)
-X_test_transformed = scaler.transform(X_test)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
 ros = RandomOverSampler(random_state=1)
-X_train_resampled, y_train_resampled = ros.fit_resample(X_train_fit, y_train)
+X_train_resampled, y_train_resampled = ros.fit_resample(X_train_scaled, y_train)
 
 model = KNeighborsClassifier(n_neighbors=7)
 model.fit(X_train_resampled, y_train_resampled)
 
-print(classification_report(y_test, model.predict(X_test)))
+print(classification_report(y_test, model.predict(X_test_scaled)))
 
-joblib.dump(model, "knn_pipeline.joblib")
+joblib.dump({"scaler": scaler, "model": model}, "knn_model.joblib")
